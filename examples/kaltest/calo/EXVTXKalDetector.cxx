060b8a1426b61c2a8f856a418308724b8fbcde8e
siyu-chen <a0119544@nus.edu.sg>
2016-06-01 11:22:03 +0200

    Deleted the commented statements of EXVTXKalDetector.cxx

--- examples/kaltest/calo/EXVTXKalDetector.cxx
+++ examples/kaltest/calo/EXVTXKalDetector.cxx
@@ -105,56 +105,6 @@ EXVTXKalDetector::EXVTXKalDetector(Int_t m)
 	   yDepth += ecalBarrelLayerLayout.getThickness(l);
 	   xiWidth = 2 * yDepth * tan(TMath::Pi()/8);
 	}
-#if 0 
-   for (Int_t layer = 0; layer < _nLayer; layer++) {
-     Int_t    nladder   = _geodata[layer].nladder;
-     Double_t rmin      = _geodata[layer].rmin;
-     Double_t sximin    = _geodata[layer].sximin;
-     Double_t sximax    = _geodata[layer].sximax;
-     Double_t sxiwidth  = sximax-sximin;
-     Double_t xioffset  = (sximin+sximax)/2;
-     Double_t hlength   = _geodata[layer].hlength;
-     Double_t lyrthick  = _geodata[layer].lyrthick;
-     Double_t plyhwidth = rmin*TMath::Tan( TMath::Pi() / nladder );
-     static const Double_t eps = 1e-6; 
-     
-     for(Int_t ladder = 0; ladder < nladder; ladder++){
-       Double_t cosphi  =_geodata[layer].cosphi[ladder];
-       Double_t sinphi  =_geodata[layer].sinphi[ladder];     
-       
-       TVector3 xc( rmin*cosphi, rmin*sinphi, 0); 
-       TVector3 normal( cosphi, sinphi, 0);
-       std::stringstream ss;
-       ss << "VTX" << "_ly" << layer << "_ld" << ladder << std::ends;
-
-       Bool_t inner = activeness[layer%2];
-       Bool_t outer = activeness[(layer+1)%2];
-
-       if(layer%2 == 0 ){ // Side drop of ladder0 is defined after the last ladder,
-	 if(ladder==0){   // bacause Side drop of ladder0 is outer than the last ladder.
-
-	   Add(new EXVTXMeasLayer(air, si, xc, normal, rmin + 2*ladder*eps, plyhwidth - sximin, 2*hlength, (plyhwidth + sximin)/2, sigmaxi, sigmazeta, inner,ss.str().data()));       
-	   xc.SetXYZ( (rmin+lyrthick)*cosphi, (rmin+lyrthick)*sinphi, 0); 
-	   Add(new EXVTXMeasLayer(si, air, xc, normal, rmin + (2*ladder+1)*eps, plyhwidth - sximin, 2*hlength, (plyhwidth + sximin)/2, sigmaxi, sigmazeta, outer));	  	 
-	   
-	   xc.SetXYZ( rmin*cosphi, rmin*sinphi, 0); 
-	   Add(new EXVTXMeasLayer(air, si, xc, normal, rmin + 2*nladder*eps, sxiwidth + sximin - plyhwidth, 2*hlength, (sximax + plyhwidth)/2, sigmaxi, sigmazeta, inner,ss.str().data()));       
-	   xc.SetXYZ( (rmin+lyrthick)*cosphi, (rmin+lyrthick)*sinphi, 0); 
-	   Add(new EXVTXMeasLayer(si, air, xc, normal, rmin + (2*nladder+1)*eps, sxiwidth + sximin -plyhwidth, 2*hlength, (sximax + plyhwidth)/2, sigmaxi, sigmazeta, outer));
-	 }else{
-	   
-	   Add(new EXVTXMeasLayer(air, si, xc, normal, rmin + 2*ladder*eps, sxiwidth, 2*hlength, xioffset, sigmaxi, sigmazeta, inner,ss.str().data()));       
-	   xc.SetXYZ( (rmin+lyrthick)*cosphi, (rmin+lyrthick)*sinphi, 0); 
-	   Add(new EXVTXMeasLayer(si, air, xc, normal, rmin + (2*ladder+1)*eps, sxiwidth, 2*hlength, xioffset, sigmaxi, sigmazeta, outer));	  	 
-	 }	 
-       }else{
-	 Add(new EXVTXMeasLayer(air, si, xc, normal, rmin + 2*ladder*eps, sxiwidth, 2*hlength, xioffset, sigmaxi, sigmazeta, inner,ss.str().data()));       
-	 xc.SetXYZ( (rmin+lyrthick)*cosphi, (rmin+lyrthick)*sinphi, 0); 
-	 Add(new EXVTXMeasLayer(si, air, xc, normal, rmin + (2*ladder+1)*eps, sxiwidth, 2*hlength, xioffset, sigmaxi, sigmazeta, outer));
-       }
-     }
-   }
-#endif
    SetOwner();			
 }
 
